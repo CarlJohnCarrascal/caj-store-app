f3c3697bffa7fd815456acf3e59ceb3e11cb83e4
Carl John Carrascal <carrascalcarljohn1@gmail.com>
2025-06-21 15:49:29 +0000

    resize the product grid item 2 per row for mobile, 3 per row for tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
       
       {filteredProducts.length > 0 ? (
         viewMode === 'grid' ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
             {filteredProducts.map(product => (
               <ProductCard key={product.id} product={product} />
             ))}
